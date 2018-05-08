8a9533f969f2913bf2e28540da06aa624f154966
nikita <nikita@real.me>
2018-05-08 17:44:30 +0300

    slideshow

--- application.js
+++ application.js
@@ -1,7 +1,8 @@
 App.onLaunch = function(options) {
-   var template = '<document><loadingTemplate><activityIndicator><text>Hello World!</text></activityIndicator></loadingTemplate></document>';
-   var templateParser = new DOMParser();
-   var parsedTemplate = templateParser.parseFromString(template, "application/xml");
-   navigationDocument.pushDocument(parsedTemplate);
+   function mySlideshow() {
+      var images = ['https://e-oboi.com/wp-content/uploads/2017/10/stock-photo-picturesque-morning-view-of-plitvice-national-park-colorful-spring-scene-of-green-forest-with-pure-717874486.jpg.pagespeed.ce.X2rWFjZqSb.jpg', 'https://e-oboi.com/wp-content/uploads/2018/03/1268952669.jpg'];
+      Slideshow.start(images, { 'showSettings': '0' });
+   }
+   mySlideshow();
 }
 
